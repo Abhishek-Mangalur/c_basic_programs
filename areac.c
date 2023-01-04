b33f22bb1a9c5d5e79e8725b87757d0117801b92
abhishek <ahishekm.21.beis@acharya.ac.in>
2023-01-04 18:28:04 +0530

    Area of a circle

---
+++ areac.c
@@ -0,0 +1,11 @@
+// Area of circle
+
+#include <stdio.h>
+#define PI 3.14
+void main()
+{
+    float r;
+    printf("Enter the radius:\n");
+    scanf("%f",&r);
+    printf("Area of circle = %f", PI*r*r);
+}
