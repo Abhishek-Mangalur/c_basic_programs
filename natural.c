9101da9560957b773c472d929b2d28ff9b825740
abhishek <ahishekm.21.beis@acharya.ac.in>
2023-01-07 14:50:01 +0530

    To find n natural no.

---
+++ natural.c
@@ -0,0 +1,13 @@
+// To find first n natural no.
+
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the n natural no.: ");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\n",i);
+    }
+}
