fe98588f7b92729b2cad1340f7f1db6cb39a6604
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-20 08:44:43 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void main()
 {
-    int a,b,sum;
-    a = 100;
+    int a,b,sum;    // initialization of variable 
+    a = 100;    // declaring a variable
     b = 200;
-    sum = a + b;
-    printf("The Sum is = %d",sum);
+    sum = a + b;  // calculating expression  
+    printf("The Sum is = %d",sum);  // printing result
 }
