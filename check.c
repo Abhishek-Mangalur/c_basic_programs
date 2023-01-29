4818e69df82172027003637a0e311f7a7f1e05e2
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-29 07:58:15 +0530

    Checking the no. is positive or not

--- check.c
+++ check.c
@@ -7,7 +7,7 @@ void main()
     printf("Enter the number: \n");
     scanf("%d", &n);    // intake a no.
     if(n == 0)
-        printf("The no. is neither positive nor negative);
+        printf("The no. is neither positive nor negative");
     if(n > 0)       // checking the no. is positive or not
         printf("Number is positive\n");
     if(n < 0)       // checking the no. is negative
