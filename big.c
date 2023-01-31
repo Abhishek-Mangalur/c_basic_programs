7c6e2436f5eadd9b4e52069c13226c7f3aa426e2
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-31 16:07:45 +0530

    Biggest of two no.

--- big.c
+++ big.c
@@ -5,9 +5,9 @@ void main()
 {
     int a, b, big;  // declaration of variable 
     printf("Enter the value of a:\n");
-    scanf("%d",&a);     // intake of 1st no.
+    scanf("%d",&a);     // reading 1st no.
     printf("Enter the value of b:\n");
-    scanf("%d",&b);     // intake of 2nd no.
+    scanf("%d",&b);     // reading 2nd no.
     big = (a>b) ? a : b;    // checking which no. is big?
     printf("The bigger no is: %d\n", big);  // printing biggest no.
 }
