ff80e084b6aa0a3a016d19e0dd337c2660596fab
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-31 16:09:26 +0530

    Check the no. is +ve or -ve

--- check.c
+++ check.c
@@ -5,7 +5,7 @@ void main()
 {
     int n;      // declaration of variable
     printf("Enter the number: \n");
-    scanf("%d", &n);    // intake a no.
+    scanf("%d", &n);    // reading a no.
     if(n == 0)
         printf("The no. is neither positive nor negative");
     if(n > 0)       // checking the no. is positive or not
