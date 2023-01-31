68a468388fd5cca10687482d6ff65486002d137b
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-01-31 16:08:30 +0530

    Biggest of three no.

--- big1.c
+++ big1.c
@@ -5,7 +5,7 @@ void main( )
 {
     int a, b, c, large;     // variable declaration
     printf("Enter three numbers:\n");  
-    scanf("%d%d%d", &a, &b, &c);     // intake of three values
+    scanf("%d%d%d", &a, &b, &c);     // reading three values
     large = (a>b) ? ((a>c) ? a : c) : ((b>c) ? b : c);      // calculating
     printf("The largest number is: %d \n", large);      // printing result
 }
