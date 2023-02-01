437037b16749176ea36ff20db4eca57e5980b278
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-01 18:13:14 +0530

    Factorial of a no.

--- fact.c
+++ fact.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 void main()
 {
-    int n,i,fact;
+    int n,i,fact;       // declaration of variagle
     printf("Enter the value of n: ");
-    scanf("%d", &n);
+    scanf("%d", &n);        // reading a no.
     fact = 1;
     i = 1;
     while(i<=n)
     {
-        fact = fact * i;
+        fact = fact * i;    // calculating the facorial
         i = i + 1;
     }
-    printf("Factorial of %d is %d\n", n, fact);
+    printf("Factorial of %d is %d\n", n, fact);     // print the factorial result
 }
