909cd8834fc5732d29838e723bbd0f2c5980679c
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-02 15:25:39 +0530

    Grading system

--- grade.c
+++ grade.c
@@ -3,23 +3,23 @@
 #include<stdio.h>
 void main()
 {
-    int marks;
+    int marks;      // variable declaration
     printf("Enter the marks:\n");
-    scanf("%d",&marks);
-    if(marks <= 39)
-        printf("Grade F (Fail)\n");
-    else if(marks <= 49)
-        printf("Grade E\n");
-    else if(marks <= 59)
-        printf("Grade D\n");
-    else if(marks <= 69)
-        printf("Grade C\n");
-    else if(marks <= 79)
-        printf("Grade B\n");
-    else if(marks <= 89)
-        printf("Grade A\n");
-    else if(marks <= 100)
-        printf("Grade O (Outstanding)\n");
+    scanf("%d",&marks);     // reading the marks
+    if(marks <= 39)     // condition checking
+        printf("Grade F (Fail)\n");     // printing the grade
+    else if(marks <= 49)        // condition checking
+        printf("Grade E\n");        // printing the grade
+    else if(marks <= 59)        // condition checking
+        printf("Grade D\n");        // printing the grade
+    else if(marks <= 69)        // condition checking
+        printf("Grade C\n");        // printing the grade
+    else if(marks <= 79)        // condition checking
+        printf("Grade B\n");        // printing the grade        
+    else if(marks <= 89)        // condition checking
+        printf("Grade A\n");        // printing the grade
+    else if(marks <= 100)       // condition checking
+        printf("Grade O (Outstanding)\n");      // printing the grade
     else
-        printf("You have entered more than 100 marks\n");
+        printf("You have entered more than 100 marks\n");   
 }
