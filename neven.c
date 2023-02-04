5b6f3a066b203d39b9a729643b1fb62b97c747b8
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-04 09:13:20 +0530

    To print 'n' even no.

--- neven.c
+++ neven.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 int main()
 {
-    int i = 2,n;
+    int i = 2,n;        // declaration of variable
     printf("Enter the no. upto you want to see: ");
-    scanf("%d",&n);
+    scanf("%d",&n);     // reading the no.
     while (i<=n)
     {
-        printf("%d\n",i);
+        printf("%d\n",i);       // printing the the even no.
         i += 2;
     }
     return 0;
