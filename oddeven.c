fd827bb5a4f83b5184da79ddb330ec7537304f9d
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-06 15:40:30 +0530

    To find the no. is odd or even

--- oddeven.c
+++ oddeven.c
@@ -1,13 +1,13 @@
 // To check the no. is odd or even 
 
-#include<stdio.h>
+#include <stdio.h>
 void main()
 {
-    int n;
+    int n;      // variable declaration
     printf("Enter the number: \n");
-    scanf("%d", &n);
-    if(n % 2 == 0)
-    printf("Number is even\n");
-    if(n % 2 != 0)
-    printf("Number is odd\n");
+    scanf("%d", &n);        // reading variable
+    if(n % 2 == 0)      // even condition checking
+    printf("Number is even\n");     // priting the even no.
+    if(n % 2 != 0)      // odd condition checking
+    printf("Number is odd\n");      // printing the odd no.
 }
