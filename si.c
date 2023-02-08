e8e1fd7209107ca89a80df3b3c69567ce941531b
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-08 19:44:10 +0530

    Simple Interest

--- si.c
+++ si.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 void main()
 {
-    int a,b,c;
+    int a,b,c;      // declaration of variable
     float si;
     printf("Enter the p,t,r:\n");
-    scanf("%d%d%d",&a,&b,&c);
-    si = (a*b*c)/100;
-    printf("Simple Interest is = %f",si);
+    scanf("%d%d%d",&a,&b,&c);       // reading values
+    si = (a*b*c)/100;       // condition checking
+    printf("Simple Interest is = %f",si);       // printing result
 }
