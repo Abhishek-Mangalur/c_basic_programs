11db8f64c50e54c2539dc568b5b28b5f742c6290
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-09 20:04:32 +0530

    Sum of the series

--- sos.c
+++ sos.c
@@ -4,18 +4,18 @@
 #include <math.h>
 int main()
 {
-    int n, x, sum, i;
+    int n, x, sum, i;       // declaration of variable
     printf("Enter the value of x:\t");
-    scanf("%d", &x);
+    scanf("%d", &x);        // reading coefficient
     printf("Enter the value of n and it should be greater than 0:\t");
-    scanf("%d",&n);
+    scanf("%d",&n);         // reading the power of coefficient
     sum = 1;
     i = 1;
     while(i<=n)
     {
-        sum += pow(x,i);
-        i = i + 1;
+        sum += pow(x,i);        // calculating all sum
+        i = i + 1;          // incrementing next value
     }
-    printf("sum of series is %d", sum); 
+    printf("sum of series is %d", sum);         // printing the sum
     return 0;
 }
