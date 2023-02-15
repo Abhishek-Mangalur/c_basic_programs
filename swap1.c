b46008445788107609d314ecbeff1155e3887e05
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-15 18:37:34 +0530

    Swaping two no. without using temp variable

--- swap1.c
+++ swap1.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 void main()
 {
-    int a,b;
+    int a,b;        // declaration of variable
     printf("Enter two numbers:\n");
-    scanf("%d%d",&a,&b);
+    scanf("%d%d",&a,&b);        // reading two no.
     printf("The numbers before swap: \nnum1 = %d \nnum2 = %d\n",a,b);
-    a = a + b;
+    a = a + b;          // swaping the no.
     b = a - b;
     a = a - b;
-    printf("The numbers after swap: \nnum1 = %d \nnum2 = %d\n",a,b);
+    printf("The numbers after swap: \nnum1 = %d \nnum2 = %d\n",a,b);        // printing the result
 }
