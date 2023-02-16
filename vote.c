ed937b12202ecda62ad4020c173a87069843ad45
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2023-02-16 18:16:28 +0530

    Voting Eligibility Tester

--- vote.c
+++ vote.c
@@ -1,13 +1,13 @@
 // Voting eligibility check
 
 #include<stdio.h>
-void main( )
+void main()
 {
-    int age;
+    int age;        // declaration of variable
     printf("Enter the age:\n");
-    scanf("%d",&age);
-    if(age >= 18)
-        printf("Person is eligible for voting\n");
+    scanf("%d",&age);           // reading the age
+    if(age >= 18)               // condition checking 
+        printf("Person is eligible for voting\n");          // printing eligible result
     if(age < 18)
-        printf("Person is not eligible for voting\n");
+        printf("Person is not eligible for voting\n");      // printing non eligible result
 }
